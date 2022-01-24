899c2f1477355d144b52a870e12bc8390b2760eb
Sarah Melanie White <sawrrawr@gmail.com>
2022-01-24 18:39:39 -0330

    added some folders, 'views', views/pages, views/partials based on an ejs tutorial and estimating what might be useful for this project

--- express_server.js
+++ express_server.js
@@ -2,23 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//setting the view engine to ejs
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// index page
 app.get("/", (req, res) => {
-  res.send(`Hello`);
+  // res.render('pages/index');
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  // res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-  res.send(`<html><body>Hello <b>World</b></body></html>\n`);
+  // res.send(`<html><body>Hello <b>World</b></body></html>\n`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+//setting the port & message
+app.listen(PORT);
+console.log(`Server is listening on port ${PORT}`);
