3d42fe782d0da3413e5077349e5eda1ec907191a
Sarah Melanie White <sawrrawr@gmail.com>
2022-01-26 16:49:11 -0330

    created a registration page, haven't implemented the POST functionality for it yet

--- express_server.js
+++ express_server.js
@@ -47,6 +47,17 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 })
 
+//register for an account
+app.get('/register', (req, res) => {
+  const templateVars = { username: req.cookies.username, }
+  res.render("register", templateVars)
+});
+
+app.post('/register', (req, res) => {
+
+
+});
+
 // list of URLs and their corresponding shortURLS
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies.username,};
